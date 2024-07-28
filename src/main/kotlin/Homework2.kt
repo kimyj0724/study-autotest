0e1c35af04de4c5d32921a255adfcee5138003f9
kimyj <kimyj@banksalad.com>
2024-07-28 18:31:33 +0900

    주석 추가

--- src/main/kotlin/Homework2.kt
+++ src/main/kotlin/Homework2.kt
@@ -6,6 +6,7 @@ class Homework2 {
         val numArr = ArrayList<Int>()
 
         strArr.indices.forEach {
+            // indices : Collection 타입이 갖는 속성으로 IntRange 타입 반환
             numArr.add(strArr[it].toInt())
         }
         return "${numArr.minOrNull()} ${numArr.maxOrNull()}"
