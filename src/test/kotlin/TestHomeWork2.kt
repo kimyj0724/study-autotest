ae07e10b5b6bb00ac133420591eb20992f761b80
kimyj <kimyj@banksalad.com>
2024-08-06 19:12:28 +0900

    testNG, testClass 추가

---
+++ src/test/kotlin/TestHomeWork2.kt
@@ -0,0 +1,21 @@
+import org.example.Homework2
+import org.testng.annotations.Test
+
+class TestHomeWork2 {
+    val HW2 = Homework2()
+
+    @Test
+    fun testMinMax() {
+        println("1")
+    }
+
+    @Test
+    fun testMinMax_() {
+        println("2")
+    }
+
+    @Test
+    fun testMinMax__() {
+        println("3")
+    }
+}
