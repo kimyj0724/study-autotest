17569cadf4e86c3ead9585834260a4d7dcebf2de
kimyj <kimyj@banksalad.com>
2024-08-07 10:59:19 +0900

    HW2 test 추가

--- src/main/kotlin/Homework2.kt
+++ src/main/kotlin/Homework2.kt
@@ -9,10 +9,15 @@ class Homework2 {
         val strArr = s.split(" ")
         val numArr = ArrayList<Int>()
 
-        strArr.indices.forEach {
-            // indices : Collection 타입이 갖는 속성으로 IntRange 타입 반환
-            numArr.add(strArr[it].toInt())
+        try {
+            strArr.indices.forEach {
+                // indices : Collection 타입이 갖는 속성으로 IntRange 타입 반환
+                numArr.add(strArr[it].toInt())
+            }
+        } catch (e: Exception) {
+            return "올바르지 않은 Input 입니다."
         }
+
         return "${numArr.minOrNull()} ${numArr.maxOrNull()}"
     }
 }
--- src/test/kotlin/TestHomeWork2.kt
+++ src/test/kotlin/TestHomeWork2.kt
@@ -1,21 +1,22 @@
 import org.example.Homework2
 import org.testng.annotations.Test
+import kotlin.test.assertEquals
 
 class TestHomeWork2 {
     val HW2 = Homework2()
 
     @Test
-    fun testMinMax() {
-        println("1")
+    fun testMinMaxEqualMinMaxInput() {
+        assertEquals("0 0", HW2.solution("0 0 0 0 0"))
     }
 
     @Test
-    fun testMinMax_() {
-        println("2")
+    fun testMinMaxMixedOrderInput() {
+        assertEquals("-33 100", HW2.solution("2 100 -10 68 -33"))
     }
 
     @Test
-    fun testMinMax__() {
-        println("3")
+    fun testMinMaxEmptyInput() {
+        assertEquals("올바르지 않은 Input 입니다.", HW2.solution(""))
     }
 }
